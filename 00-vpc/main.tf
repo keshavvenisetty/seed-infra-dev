472c8422a22f90c703b4e14aa103a21f2aa8de4f
Keshavvenisetty <keshavvenisetty.com>
2026-01-08 13:46:58 +0530

    terraform-seed-infra-dev

---
+++ 00-vpc/main.tf
@@ -0,0 +1,16 @@
+module "vpc"{
+    #source = "../terraform-aws-vpc"  
+    source = "git::https://github.com/keshavvenisetty/terraform-aws-vpc.git?ref=main"
+    /* project =  "roboshop"
+    environment = "dev"
+    public_subnet_cidr = [ "10.0.1.0/24", "10.0.2.0/24" ] */
+    project = var.project
+    environment = var.environment
+    public_subnet_cidr = var.public_subnet_cidr
+    private_subnet_cidr = var.private_subnet_cidr
+    database_subnet_cidr = var.database_subnet_cidr
+
+    is_peering_required = true
+
+}
+
